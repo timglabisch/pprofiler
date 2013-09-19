4742fb6effa18a4bb9eeb9d3902586aecf134016
dev <dev@ubuntu.(none)>
2013-09-19 14:14:39 -0700

    + unittests

--- tests/tests/Pprofiler/ProfilerTest.php
+++ tests/tests/Pprofiler/ProfilerTest.php
@@ -15,17 +15,44 @@ class ProfilerTest extends \PHPUnit_Framework_TestCase {
         $this->profiler = new Profiler($timer);
     }
 
+    /** @return \Pprofiler\Timer */
+    function getProfilerMock() {
+        return $this->getMock('\Pprofiler\Timer');
+    }
+
     function testFoo() {
-        $this->profiler->start('foo');
-        $this->profiler->start('foo');
+
+        $timer = $this->getProfilerMock();
+        $getTime = $timer->expects($this->any())->method('getTime');
+        $getTime->will($this->returnValue(0.1));
+
+
+        $this->profiler = new Profiler($timer);
+        $this->profiler->start('foo1');
+        $getTime->will($this->returnValue(0.15));
         $this->profiler->start('foo2');
-        sleep(1);
+        $getTime->will($this->returnValue(0.2));
 
         $this->profiler->start('foo3');
+        $this->profiler->start('foo4');
+        $getTime->will($this->returnValue(0.3));
+
+
+        $buffer = array();
 
         foreach($this->profiler as $t)
             /** @var $t TimeSpan */
-            echo $t->getName() .': '. $t->getRealTime()."\n";
+            $buffer[] = array($t->getName(), $t->getRealTime());
+
+        $this->assertEquals(
+            array(
+                array('foo1', 0.2),
+                array('foo2', 0.15),
+                array('foo3', 0.1),
+                array('foo4', 0.1),
+            ),
+            $buffer
+        );
     }
 
 }
